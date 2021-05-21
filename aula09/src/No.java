93cb10c04eeaa9f2189d551ee5b3aa31266c348e
EmersonPaduan <emerson.paduan@gmail.com>
2021-05-21 20:10:25 -0300

    classe queue implementada

--- aula09/src/No.java
+++ aula09/src/No.java
@@ -1,3 +1,21 @@
 public class No {
-    
+    private Client client;
+    private No prox;
+
+    public No(String name, int age) {
+        client = new Client(name, age);
+        prox = null;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
 }
--- aula09/src/Queue.java
+++ aula09/src/Queue.java
@@ -1,3 +1,58 @@
 public class Queue {
-    
+    private No inicio, fim;
+
+    public Queue() {
+        inicio = fim = null;
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public void enqueue(String name, int age) {
+        No novo = new No(name, age);
+
+        if(isEmpty()) {
+            inicio = fim = novo;
+        }else{
+            //aponta o próximo do último para o novo No
+            fim.setProx(novo);
+            // movo o "fim" para o novo No
+            fim = novo;
+
+        }
+    }
+
+    public String show() {
+        String saida = "Fila: ";
+        No aux = inicio;
+
+        while( aux != null) {
+            saida += aux.getClient().getName() + ", ";
+            aux = aux.getProx();
+        }
+        return saida;
+    }
+
+    public String peek() { 
+        if(isEmpty()) {
+            return "";
+        }
+        return inicio.getClient().getName();
+    }
+
+    public String dequeue() {
+        if(isEmpty()) {
+            return "";
+        }
+        No aux = inicio;
+        // aponta o "inicio" para o próximo No
+        inicio = inicio.getProx();
+
+        if(inicio == null) { // se a fila ficou vazia
+            fim = null;
+        }
+
+        return aux.getClient().getName();
+    }
 }
