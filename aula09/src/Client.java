3fbdf42aa3cb26b8ff03eb509db4f2c23a24a758
EmersonPaduan <emerson.paduan@gmail.com>
2021-05-21 20:00:26 -0300

    classe client implementada

---
+++ aula09/src/Client.java
@@ -0,0 +1,40 @@
+
+public class Client {
+    private String name;
+    private int age;
+    
+
+    public Client(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+
+    private void setName(String name) {
+        String novoNome = name.trim().replace("\\s{2,}", " ");
+        if(novoNome.length() >= 5) {
+            this.name = novoNome;
+        }
+    }
+
+    private void setAge(int age) {
+        if( age > 15) {
+            this.age = age;
+        } else {
+            this.age = 18;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isElderly() {
+        return age >= 60;
+    }
+    
+}
